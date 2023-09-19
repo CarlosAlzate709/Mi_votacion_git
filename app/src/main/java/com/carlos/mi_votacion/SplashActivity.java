10beeb7eead90ff36d323bd3487bcdd5d4e8bc94
CarlosAlzate709 <110996163+CarlosAlzate709@users.noreply.github.com>
2023-09-18 20:18:41 -0500

    Final

--- app/src/main/java/com/carlos/mi_votacion/SplashActivity.java
+++ app/src/main/java/com/carlos/mi_votacion/SplashActivity.java
@@ -5,20 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    TextView nombre;
+    String nombreText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
-            }
-        },3000);
     }
 }
